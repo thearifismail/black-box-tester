095126c07dd12da3030093e38351d9e49b0485a1
Brandon Squizzato <bsquizza@redhat.com>
2019-10-18 20:30:20 -0400

    Disable non-working plugins, reduce default runners to 1

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ IQE_PASSWORD = os.getenv("IQE_PASSWORD").strip()
 IQE_ACCT_NO = os.getenv("IQE_ACCOUNT_NUMBER").strip()
 IQE_TESTS_LOCAL_CONF_PATH = os.environ.get("IQE_TESTS_LOCAL_CONF_PATH", _this_dir)
 ENV_FOR_DYNACONF = os.environ.get("ENV_FOR_DYNACONF", "prod")
-MAX_RUNNERS = os.environ.get("MAX_RUNNERS", 2)
+MAX_RUNNERS = os.environ.get("MAX_RUNNERS", 1)
 IBUTSU_SERVER = os.environ.get("IBUTSU_SERVER", "https://ibutsu-api.cloud.paas.psi.redhat.com")
 IBUTSU_SOURCE = os.environ.get("IBUTSU_SOURCE", "blackbox")
 
@@ -23,18 +23,18 @@ if SINGLE_PLUGIN:
 else:
     PLUGINS = [
         # (Plugin name, pytest marker)
-        ("akamai", None),
-        ("advisor", "prod_status"),
+        #("akamai", None),
+        #("advisor", "prod_status"),
         ("platform_ui", "prod_status"),
         ("approval", "prod_status"),
         ("sources", "prod_status"),
         ("topology_inventory", "prod_status"),
-        ("clientv3", "prod_status"),
-        ("compliance", "prod_status"),
-        ("drift", "prod_status"),
-        ("hccm", "prod_status"),
+        #("clientv3", "prod_status"),
+        #("compliance", "prod_status"),
+        #("drift", "prod_status"),
+        #("hccm", "prod_status"),
         ("host_inventory", "prod_status"),
-        ("rbac", "prod_status"),
-        ("upload", "prod_status"),
+        #("rbac", "prod_status"),
+        #("upload", "prod_status"),
         ("vulnerability", "prod_status"),
     ]
