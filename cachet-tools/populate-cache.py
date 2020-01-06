338d9de8bad4bb9eaa4e51545811de48a43faeb2
thearifismail <arifismail@yahoo.com>
2020-01-06 10:56:00 -0500

    added supported services and set their starting status to unknown

--- cachet-tools/populate-cache.py
+++ cachet-tools/populate-cache.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import logging
 
 CACHET_HOSTNAME = os.environ.get("CACHET_HOSTNAME")
 URL = f"https://{CACHET_HOSTNAME}/api/v1/components"
@@ -9,6 +10,11 @@ HEADERS = {
     'X-Cachet-Token': os.environ.get("CACHET_TOKEN")
 }
 
+# setup log format
+logging.basicConfig(level='INFO', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p: ')
+logger = logging.getLogger(__name__)
+
+'''
 groups = {
     "Cloud Services Applications": [
         "Cost Management",
@@ -62,20 +68,52 @@ groups = {
         "Entitlements"
     ]
 }
+'''
 
+groups = {
+    "Cloud Services Applications": [
+        "webhooks",
+        "vulnerability",
+        "tower-analytics",
+        "topological-inventory",
+        "system-baseline",
+        "subscriptions",
+        "sources",
+        "rhsm-subscriptions",
+        "remediations",
+        "rbac",
+        "inventory",
+        "insights",
+        "ingress",
+        "hooks",
+        "entitlements",
+        "echo",
+        "drift",
+        "cost-anagement",
+        "compliance",
+        "catalog",
+        "automation-hub",
+        "approval",
+        "apicast-tests"
+    ]
+}
 with requests.Session() as session:
     session.headers.update(HEADERS)
+
     for group, services in groups.items():
-        print(f"Creating group {group}")
+        logger.info(f"Creating group {group}")
         r = session.post(URL + "/groups", verify=False, json={
             "name": group,
-            "collapsed": 1
+            "collapsed": 0,
+            "visible": 1
         })
         group_id = r.json()["data"]["id"]
         for service in services:
-            print(f"Creating service {service} under group {group} ({group_id})")
+            logger.info(f'Creating service "{service}" under group "{group}" ({group_id})')
             r = session.post(URL, verify=False, json={
                 "name": service,
                 "group_id": group_id,
-                "status": 1
+                # set new service status to Unknown
+                # options are: 0=Unknown, 1=Operational, 2=Performance Issues , 3=Partial Outage , 4=Major Outage
+                "status": 0
             })
